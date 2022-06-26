5f4089c7e32294f71b230480dfdc02e112a0a264
joudisoft <69197182+joudisoft@users.noreply.github.com>
2022-06-26 20:27:40 +0300

    refresh al

---
+++ assets/js/learnCustom.js
@@ -0,0 +1,287 @@
+$(function () {
+  let to_up = $(".toUp");
+  let rootColor = $(":root");
+
+  let gamesCSharp = $(".g_cSharp");
+  let gamesCpp = $(".g_cPlusPlus");
+  let gamesPy = $(".g_python");
+  let gamesJava = $(".g_java");
+  let gamesJavascript = $(".g_javascript");
+  let game_cSharpSelected = $(".g_cSharpSelected");
+  let game_cPlusPlusSelected = $(".g_cPlusPlusSelected");
+  let game_pythonSelected = $(".g_pythonSelected");
+  let game_javaSelected = $(".g_javaSelected");
+  let game_javascriptSelected = $(".g_javascriptSelected");
+  
+  let nav_webSection = $(".nav-webSection");
+  let nav_mobileSection = $(".nav-mobileSection");
+  let nav_desktopSection = $(".nav-desktopSection");
+  let nav_databaseSection = $(".nav-databaseSection");
+  let nav_gamesSection = $(".nav-gamesSection");
+  let nav_systemSection = $(".nav-systemSection");
+  let nav_machineSection = $(".nav-machineSection");
+
+  // ========== Start side Menu Section =================== //
+  let side_menu = $(".sideMenu");
+  let sideMenu_Show = $("#sideMenuShow");
+  let sideMenuItem = $(".sideMenu_item");
+  let sub_items = $(".sideMenu_subItems");
+
+  sideMenu_Show.click(function () {
+    side_menu.animate({ right: "0px" }, 200);
+    $(".fullOverrideBlack").css("opacity", "1");
+    $(this).hide();
+  });
+  $(".mobileMenuIcon").click(function () {
+    side_menu.animate({ right: "0px" }, 200);
+    $(this).css("visibility", "hidden")
+  });
+
+  $("#sideMenuHeader_close").click(function () {
+    side_menu.animate({ right: "-100%" }, 200);
+    $(".mobileMenuIcon").css("visibility", "visible");
+    sub_items.slideUp();
+    $(".fullOverrideBlack").css("opacity", "0");
+    sideMenu_Show.show();
+  });
+
+  sideMenuItem.click(function () {
+    const this_title = $(this);
+    const this_content = this_title.find(sub_items);
+    this_content.slideToggle();
+    sub_items.not(this_content).slideUp();
+  });
+  // =========== End Slide Bar Menu Section ============================== //
+
+  // ==================== Start Color Theme Section ========================== //
+  $(".colorStyleIcon").click(function () {});
+  
+  $(".colorStyleIcon").click(function () {
+    $("#testStyle").slideToggle(200);
+  });
+
+  $(".Default").click(function () {
+    rootColor.css("--main-color", "#1987af");
+    $(":root").css("--main-txt-color", "#fff");
+    $(this).addClass("selected").siblings().removeClass("selected");
+  });
+
+  $(".Red").click(function () {
+    rootColor.css("--main-color", "#900000");
+    rootColor.css("--main-txt-color", "#fff");
+    $(this).addClass("selected").siblings().removeClass("selected");
+  });
+
+  $(".Green").click(function () {
+    rootColor.css("--main-color", "#198754");
+    rootColor.css("--main-txt-color", "#fff");
+    $(this).addClass("selected").siblings().removeClass("selected");
+  });
+
+  $(".Gold").click(function () {
+    rootColor.css("--main-color", "#FFD700");
+    rootColor.css("--main-txt-color", "#000");
+    $(this).addClass("selected").siblings().removeClass("selected");
+  });
+
+  $(".Black").click(function () {
+    rootColor.css("--main-color", "#000");
+    rootColor.css("--main-txt-color", "#fff");
+    $(this).addClass("selected").siblings().removeClass("selected");
+    to_up.css("box-shadow", " 0 0 4px 1px #fff");
+    $(".fa-angle-up").css("color", "#fff");
+    $(".hvr-sweep-to-top:before").css("background", "#000");
+  });
+
+  $(".Orange").click(function () {
+    rootColor.css("--main-color", "#dd6d00");
+    rootColor.css("--main-txt-color", "#fff");
+    $(this).addClass("selected").siblings().removeClass("selected");
+
+    
+  });
+
+  $(".Pempe").click(function () {
+    rootColor.css("--main-color", "#c200c2");
+    rootColor.css("--main-txt-color", "#fff");
+    $(this).addClass("selected").siblings().removeClass("selected");
+  });
+  // ================================================= End Color Theme Section ================================================= //
+
+$('.navLi').click(function () {
+  $(this).addClass("activeR").siblings().removeClass("activeR");
+});
+  $(window).scroll(function () {
+    if ($(this).scrollTop() >= 800) {
+      to_up.css("right", "20px");
+    } else {
+      to_up.css("right", "-42px");
+    }
+  });
+
+  // ================================================= Start Back up to top button Section ================================================= //
+  // to_up.click(function () {
+  //   // $("html, body").animate({ scrollTop: 0 });
+  //   alert('ji');
+  // });
+
+  nav_webSection.click(function (){
+    $("html, body").animate({ scrollTop: 2270 });
+  });
+
+  nav_mobileSection.click(function (){
+    $("html, body").animate({ scrollTop: 3575 });
+  });
+
+  nav_desktopSection.click(function (){
+    $("html, body").animate({ scrollTop: 4815 });
+  });
+
+  nav_databaseSection.click(function (){
+    $("html, body").animate({ scrollTop: 6570});
+  });
+
+  nav_gamesSection.click(function (){
+    $("html, body").animate({ scrollTop: 7330 });
+  });
+  
+  nav_systemSection.click(function (){
+    $("html, body").animate({ scrollTop: 7940 });
+  });
+
+  nav_machineSection.click(function (){
+    $("html, body").animate({ scrollTop: 9760 });
+  });
+
+  $(window).scroll(function () {
+    console.log($(this).scrollTop());
+    if ($(this).scrollTop() >= 100) {
+      console.log("value = " + $(this).scrollTop());
+      // $(".navCustom").css("position", "fixed");
+      $(".navCustom").css("box-shadow","0 1px 5px #555");
+      $(".navCustom").css("background-color","#fff");
+    }
+    else {
+        // $(".navCustom").css("position", "relative");
+        $(".navCustom").css("box-shadow","none");
+        $(".navCustom").css("background-color","#a1cadd");
+        $('.home').addClass("activeR").siblings().removeClass("activeR");
+      };
+
+      //>>>> Home section active
+      if ($(this).scrollTop() < 2220) {
+        $('.home').addClass("activeR").siblings().removeClass("activeR");
+        $('.overBlack_web').css("opacity", "1");
+      } 
+      //>>>> Web section active
+      else if (($(this).scrollTop() >= 2220) & ($(this).scrollTop() < 3525)) {
+        $('.webActive').addClass("activeR").siblings().removeClass("activeR");
+        $('.overBlack_web').css("opacity", "0");
+        $('.overBlack_web').css("z-index", "-1");
+        $('.overBlack_mobile').css("opacity", "1");
+      } 
+      //>>>> Mobile section active
+      else if (($(this).scrollTop() >= 3525) & ($(this).scrollTop() < 4695)) {
+        $('.mobileActive').addClass("activeR").siblings().removeClass("activeR");
+        $('.overBlack_mobile').css("opacity", "0");
+        $('.overBlack_mobile').css("z-index", "-1");
+        $('.overBlack_desktop').css("opacity", "1");
+      } 
+      //>>>> Desktop section active
+      else if (($(this).scrollTop() >= 4765) & ($(this).scrollTop() < 6500)) {
+        $('.desktopActive').addClass("activeR").siblings().removeClass("activeR");
+        $('.overBlack_desktop').css("opacity", "0");
+        $('.overBlack_desktop').css("z-index", "-1");
+        $('.overBlack_database').css("opacity", "1");
+      }
+      //>>>> Database section active
+      else if (($(this).scrollTop() >= 6500) & ($(this).scrollTop() < 7250)) {
+        $('.databaseActive').addClass("activeR").siblings().removeClass("activeR");
+        $('.overBlack_database').css("opacity", "0");
+        $('.overBlack_database').css("z-index", "-1");
+        $('.overBlack_games').css("opacity", "1");
+      }
+      //>>>> Games section active
+      else if (($(this).scrollTop() >= 7250) & ($(this).scrollTop() < 7870)) {
+        $('.gamesActive').addClass("activeR").siblings().removeClass("activeR");
+        $('.overBlack_games').css("opacity", "0");
+        $('.overBlack_games').css("z-index", "-1");
+        $('.overBlack_systemAndNetwork').css("opacity", "1");
+      }
+
+      //>>>> System and Network section active
+      else if (($(this).scrollTop() >= 7870) & ($(this).scrollTop() < 9675)) {
+        $('.systemActive').addClass("activeR").siblings().removeClass("activeR");
+        $('.overBlack_systemAndNetwork').css("opacity", "0");
+        $('.overBlack_systemAndNetwork').css("z-index", "-1");
+        $('.overBlack_machine').css("opacity", "1");
+      }
+
+      //>>>> Machine section active
+      else if (($(this).scrollTop() >= 9675)) {
+        $('.machineActive').addClass("activeR").siblings().removeClass("activeR");
+        $('.overBlack_machine').css("opacity", "0");
+        $('.overBlack_machine').css("z-index", "-1");
+      }
+
+    });
+    // ========== End Back up to top button Section =============== //
+
+    // ========== Start Games function section ============ //
+    let showGame = false;
+
+    function scrollGamesIncrement(){
+      if (showGame == false){
+        $("html, body").animate({ scrollTop: $(this).scrollTop()+235 });
+        showGame = true;
+      }
+      else{
+        $("html, body").animate({ scrollTop: $(this).scrollTop()-235 });
+        showGame = false;
+      }
+    }
+    gamesCSharp.click(function (){
+        game_cSharpSelected.slideToggle(300).siblings().slideUp();
+        scrollGamesIncrement();
+    })
+
+    gamesCpp.click(function (){
+      game_cPlusPlusSelected.slideToggle(300).siblings().slideUp();
+      scrollGamesIncrement();
+    })
+
+    gamesPy.click(function (){
+      game_pythonSelected.slideToggle(300).siblings().slideUp();
+      scrollGamesIncrement();
+    })
+
+    gamesJava.click(function (){
+      game_javaSelected.slideToggle(300).siblings().slideUp();
+      scrollGamesIncrement();
+    })
+
+    gamesJavascript.click(function (){
+      game_javascriptSelected.slideToggle(300).siblings().slideUp();
+      scrollGamesIncrement();
+    })
+    // ================================================= End Games function section ================================================= //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // ================================================= End Java Script File ================================================= //
+  });
---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// Global variables
+  // let to_up = document.querySelector(".toUp");
+  let toUp = document.querySelector(".toUp");
+  let rootColor = document.querySelector(":root");
+
+  let gamesCSharp = document.querySelector(".g_cSharp");
+  let gamesCpp = document.querySelector(".g_cPlusPlus");
+  let gamesPy = document.querySelector(".g_python");
+  let gamesJava = document.querySelector(".g_java");
+  let gamesJavascript = document.querySelector(".g_javascript");
+  let game_cSharpSelected = document.querySelector(".g_cSharpSelected");
+  let game_cPlusPlusSelected = document.querySelector(".g_cPlusPlusSelected");
+  let game_pythonSelected = document.querySelector(".g_pythonSelected");
+  let game_javaSelected = document.querySelector(".g_javaSelected");
+  let game_javascriptSelected = document.querySelector(".g_javascriptSelected");
+  
+  let nav_webSection = document.querySelector(".nav-webSection");
+  let nav_mobileSection = document.querySelector(".nav-mobileSection");
+  let nav_desktopSection = document.querySelector(".nav-desktopSection");
+  let nav_databaseSection = document.querySelector(".nav-databaseSection");
+  let nav_gamesSection = document.querySelector(".nav-gamesSection");
+  let nav_systemSection = document.querySelector(".nav-systemSection");
+  let nav_machineSection = document.querySelector(".nav-machineSection");
